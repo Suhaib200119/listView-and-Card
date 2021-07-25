132040fce9b7bc7ce36422671d18c3974c44b9f1
Suhaib200119 <sis.22.19a@gmail.com>
2021-07-25 14:38:33 +0300

    Format

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'Cars.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
-class MyAppState extends State<MyApp>{
-bool bswitch=false;
-Color cardColor=Colors.white;
-Color cardTextColor=Colors.blueAccent;
-Color cardShadowColor=Colors.blueAccent;
-  List<Cars> listCars=[
+
+class MyAppState extends State<MyApp> {
+  bool bswitch = false;
+  Color cardColor = Colors.white;
+  Color cardTextColor = Colors.blueAccent;
+  Color cardShadowColor = Colors.blueAccent;
+  List<Cars> listCars = [
     Cars("Car_1", 80000, "car1.jpeg"),
     Cars("Car_2", 90000, "car2.jpeg"),
     Cars("Car_3", 85000, "car3.jpg"),
@@ -29,84 +30,89 @@ Color cardShadowColor=Colors.blueAccent;
           title: Text("App Card"),
           actions: [
             Switch(
-              activeColor: Colors.grey,
+                activeColor: Colors.grey,
                 value: bswitch,
-                onChanged: (vbool){
-                setState(() {
-                  bswitch=vbool;
-                  if(bswitch==true){
-                    cardColor=Colors.black;
-                    cardTextColor=Colors.white;
-                    cardShadowColor=Colors.blueAccent;
-                  }else{
-                    cardColor=Colors.white;
-                    cardTextColor=Colors.blueAccent;
-                    cardShadowColor=Colors.blueAccent;
-                  }
-                });
+                onChanged: (vbool) {
+                  setState(() {
+                    bswitch = vbool;
+                    if (bswitch == true) {
+                      cardColor = Colors.black;
+                      cardTextColor = Colors.white;
+                      cardShadowColor = Colors.blueAccent;
+                    } else {
+                      cardColor = Colors.white;
+                      cardTextColor = Colors.blueAccent;
+                      cardShadowColor = Colors.blueAccent;
+                    }
+                  });
                 }),
           ],
         ),
         body: Container(
-          margin: EdgeInsets.symmetric(vertical: 10,horizontal: 5),
-          color:Colors.white,
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+          color: Colors.white,
           child: ListView.builder(
-            itemCount:listCars.length ,
-              itemBuilder: (con,index){
-                  return Card(
-                    color: cardColor,
-                    shadowColor: cardShadowColor,
-                    elevation: 10,
-                    margin: EdgeInsets.symmetric(vertical: 8,horizontal: 5),
-                    child: Container(
-                      height: 120,
-                      padding: EdgeInsets.symmetric(vertical: 5,horizontal: 5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("name: "+listCars[index].name.toString(),
-                                style: TextStyle(
+              itemCount: listCars.length,
+              itemBuilder: (con, index) {
+                return Card(
+                  color: cardColor,
+                  shadowColor: cardShadowColor,
+                  elevation: 10,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: Container(
+                    height: 120,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "name: " + listCars[index].name.toString(),
+                              style: TextStyle(
                                   color: cardTextColor,
                                   fontSize: 22,
                                   fontWeight: FontWeight.bold,
-                                  fontStyle: FontStyle.italic
-                                ),),
-                              SizedBox(height: 20,),
-                              Text("  price: "+listCars[index].price.toString(),
-                                style: TextStyle(
-                                    color: cardTextColor,
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                    fontStyle: FontStyle.italic
-                                ),),
-                            ],
-                          ),
-                          Image.asset("assets/images/"+listCars[index].nameImage.toString(),width: 150,height: 160,),
-                        ],
-                      ),
+                                  fontStyle: FontStyle.italic),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "  price: " + listCars[index].price.toString(),
+                              style: TextStyle(
+                                  color: cardTextColor,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic),
+                            ),
+                          ],
+                        ),
+                        Image.asset(
+                          "assets/images/" +
+                              listCars[index].nameImage.toString(),
+                          width: 150,
+                          height: 160,
+                        ),
+                      ],
                     ),
-                  );
-            }
-          ),
+                  ),
+                );
+              }),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: null,
           child: Icon(Icons.shopping_cart_sharp),
         ),
       ),
-
     );
   }
-
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return MyAppState();
   }
-
 }
